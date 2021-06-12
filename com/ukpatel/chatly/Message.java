efd11ad8e5006ce9308ce3bc2ab60de75fcd5fc8
urvesh254 <urveshpatel2001@gmail.com>
2021-06-12 13:10:12 +0530

    File transfer gui is added.

--- com/ukpatel/chatly/Message.java
+++ com/ukpatel/chatly/Message.java
@@ -1,27 +1,45 @@
 package com.ukpatel.chatly;
 
+import java.io.File;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
+    public static final int BUFFER_SIZE = 8192;
 
-    // Type of the messages.
+    // Types of the messages.
     public static final int USER_JOIN = 1;
     public static final int MESSAGE_SEND = 2;
-    public static final int MESSAGE_RECEIVE = 3;
-    public static final int USER_EXIT = 4;
+    public static final int FILE_SEND = 3;
+    public static final int MESSAGE_RECEIVE = 4;
+    public static final int FILE_RECEIVE = 5;
+    public static final int USER_EXIT = 6;
 
     private int messageType;
     private String message;
     private String author;
     private String time;
 
+    private File file;
+    private byte[] data;
+    private int byteRead;
+
+    // String Message
     public Message(String author, int messageType, String message) {
         this.author = author;
         this.messageType = messageType;
         this.message = message;
+        this.time = getCurrentTimeStamp();
+    }
+
+    // File Send or Receive.
+    public Message(File file, int messageType, int byteRead, byte[] data) {
+        this.file = file;
+        this.messageType = messageType;
+        this.data = data;
+        this.byteRead = byteRead;
         this.time = getCurrentTimeStamp();
     }
 
@@ -39,6 +57,18 @@ public class Message implements Serializable {
 
     public String getTime() {
         return this.time;
+    }
+
+    public File getFile() {
+        return this.file;
+    }
+
+    public byte[] getData() {
+        return this.data;
+    }
+
+    public int getByteRead() {
+        return this.byteRead;
     }
 
     private String getCurrentTimeStamp() {
--- com/ukpatel/layouts/MessagePanel.java
+++ com/ukpatel/layouts/MessagePanel.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -12,23 +13,37 @@ import javax.swing.border.EmptyBorder;
 import com.ukpatel.chatly.Message;
 
 public class MessagePanel extends JPanel {
+
+    private static final String FILE_ICON = "assets/file-48.png";
+    private static final String DOWNLOAD_ICON = "assets/download-40.png";
+
     public static final int USER_INFO = 0;
     public static final int USER_SEND = 1;
     public static final int USER_RECEIVE = 2;
 
+    private JLabel fileDownloadLabel;
+    private Message message;
+
     public MessagePanel(Message message, int messageType) {
+        this.message = message;
         this.setLayout(new BorderLayout());
 
         if (messageType == MessagePanel.USER_SEND) {
-            this.add(getSendPanel(message), BorderLayout.LINE_END);
+            if (message.getMessageType() == Message.FILE_SEND)
+                this.add(getFileSendPanel(), BorderLayout.LINE_END);
+            else
+                this.add(getSendPanel(), BorderLayout.LINE_END);
         } else if (messageType == MessagePanel.USER_RECEIVE) {
-            this.add(getReceivePanel(message), BorderLayout.LINE_START);
+            if (message.getMessageType() == Message.FILE_RECEIVE)
+                this.add(getFileReceivePanel(), BorderLayout.LINE_START);
+            else
+                this.add(getReceivePanel(), BorderLayout.LINE_START);
         } else {
-            this.add(getInfoPanel(message), BorderLayout.CENTER);
+            this.add(getInfoPanel(), BorderLayout.CENTER);
         }
     }
 
-    private JPanel getInfoPanel(Message message) {
+    private JPanel getInfoPanel() {
         JPanel infoPanel = new JPanel();
         JLabel label = new JLabel(message.getMessage());
         label.setBorder(new EmptyBorder(3, 3, 3, 3));
@@ -38,11 +53,11 @@ public class MessagePanel extends JPanel {
         return infoPanel;
     }
 
-    private JPanel getSendPanel(Message message) {
+    private JPanel getSendPanel() {
         JPanel sendPanel = new JPanel();
         sendPanel.setLayout(new BorderLayout());
 
-        JLabel msgLabel = new JLabel(getFormattedMessage(message.getMessage()));
+        JLabel msgLabel = new JLabel(getFormattedMessage(message.getMessage(), 280));
         msgLabel.setOpaque(true);
         msgLabel.setBackground(new Color(37, 211, 102));
         msgLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
@@ -59,19 +74,19 @@ public class MessagePanel extends JPanel {
         return sendPanel;
     }
 
-    private JPanel getReceivePanel(Message message) {
+    private JPanel getReceivePanel() {
         JPanel receivePanel = new JPanel();
         receivePanel.setLayout(new BorderLayout());
 
         JLabel authorLabel = new JLabel(message.getAuthor());
         authorLabel.setOpaque(true);
         authorLabel.setBorder(new EmptyBorder(2, 2, 0, 0));
-        authorLabel.setForeground(Color.RED);
+        authorLabel.setForeground(Color.WHITE);
         authorLabel.setBackground(new Color(37, 211, 102));
-        authorLabel.setFont(new Font("Time New Roman", Font.PLAIN, 11));
+        authorLabel.setFont(new Font("Time New Roman", Font.BOLD, 11));
         receivePanel.add(authorLabel, BorderLayout.PAGE_START);
 
-        JLabel msgLabel = new JLabel(getFormattedMessage(message.getMessage()));
+        JLabel msgLabel = new JLabel(getFormattedMessage(message.getMessage(), 280));
         msgLabel.setOpaque(true);
         msgLabel.setBackground(new Color(37, 211, 102));
         msgLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
@@ -88,13 +103,76 @@ public class MessagePanel extends JPanel {
         return receivePanel;
     }
 
-    private static String getFormattedMessage(String message) {
+    private static String getFormattedMessage(String message, int size) {
         StringBuilder formattedMessage = new StringBuilder();
         formattedMessage.append("<html>").append("<body>");
-        formattedMessage.append("<p style=\"width: 280px\">");
+        formattedMessage.append("<p style=\"width: " + size + "px\">");
         formattedMessage.append(message.replaceAll("\n", "<br>"));
         formattedMessage.append("</p>").append("</body>").append("</html>");
         return formattedMessage.toString();
     }
 
+    private JPanel getFileSendPanel() {
+        JPanel sendPanel = new JPanel();
+        sendPanel.setLayout(new BorderLayout());
+
+        JLabel fileLabel = new JLabel(getFormattedMessage(message.getFile().getName(), 195));
+        fileLabel.setOpaque(true);
+        fileLabel.setIcon(new ImageIcon(FILE_ICON));
+        fileLabel.setBackground(new Color(37, 211, 102));
+        fileLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        fileLabel.setBorder(new EmptyBorder(10, 5, 2, 2));
+        sendPanel.add(fileLabel, BorderLayout.CENTER);
+
+        fileDownloadLabel = new JLabel(new ImageIcon(DOWNLOAD_ICON));
+        fileDownloadLabel.setOpaque(true);
+        fileDownloadLabel.setBorder(new EmptyBorder(2, 5, 2, 10));
+        fileDownloadLabel.setBackground(new Color(37, 211, 102));
+        sendPanel.add(fileDownloadLabel, BorderLayout.EAST);
+
+        JLabel timeLabel = new JLabel(message.getTime(), SwingConstants.RIGHT);
+        timeLabel.setOpaque(true);
+        timeLabel.setBackground(new Color(37, 211, 102));
+        timeLabel.setBorder(new EmptyBorder(0, 0, 2, 2));
+        timeLabel.setFont(new Font("Time New Roman", Font.PLAIN, 11));
+        sendPanel.add(timeLabel, BorderLayout.PAGE_END);
+
+        return sendPanel;
+    }
+
+    private JPanel getFileReceivePanel() {
+        JPanel receivePanel = new JPanel();
+        receivePanel.setLayout(new BorderLayout());
+
+        JLabel authorLabel = new JLabel("Urvesh Patel");
+        authorLabel.setOpaque(true);
+        authorLabel.setBorder(new EmptyBorder(2, 2, 0, 0));
+        authorLabel.setForeground(Color.WHITE);
+        authorLabel.setBackground(new Color(37, 211, 102));
+        authorLabel.setFont(new Font("Time New Roman", Font.BOLD, 11));
+        receivePanel.add(authorLabel, BorderLayout.PAGE_START);
+
+        JLabel fileLabel = new JLabel(getFormattedMessage(message.getFile().getName(), 195));
+        fileLabel.setIcon(new ImageIcon(FILE_ICON));
+        fileLabel.setOpaque(true);
+        fileLabel.setBackground(new Color(37, 211, 102));
+        fileLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        fileLabel.setBorder(new EmptyBorder(2, 0, 0, 2));
+        receivePanel.add(fileLabel, BorderLayout.CENTER);
+
+        fileDownloadLabel = new JLabel(new ImageIcon(DOWNLOAD_ICON));
+        fileDownloadLabel.setOpaque(true);
+        fileDownloadLabel.setBorder(new EmptyBorder(2, 5, 2, 10));
+        fileDownloadLabel.setBackground(new Color(37, 211, 102));
+        receivePanel.add(fileDownloadLabel, BorderLayout.EAST);
+
+        JLabel timeLabel = new JLabel(message.getTime(), SwingConstants.RIGHT);
+        timeLabel.setOpaque(true);
+        timeLabel.setBorder(new EmptyBorder(0, 0, 2, 2));
+        timeLabel.setBackground(new Color(37, 211, 102));
+        timeLabel.setFont(new Font("Time New Roman", Font.PLAIN, 11));
+        receivePanel.add(timeLabel, BorderLayout.PAGE_END);
+
+        return receivePanel;
+    }
 }
